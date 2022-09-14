70235cb8f03318ace3141ee5b1835907e25ef011
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-14 13:04:35 +0500

    Level 1, Running Array

---
+++ Leetcode -Pool/Leetcode Easy Running Sum.js
@@ -0,0 +1,17 @@
+var runningSum = function (nums) {
+  //initialize sums array to store sum
+  let sums = [];
+
+  //brute force and iterate over nums array and then in j loop interate over nums till i to calculate running sum
+  for (let i = 0; i < nums.length; i++) {
+    //initialize sum to store running sum
+    let sum = 0;
+    for (let j = 0; j <= i; j++) {
+      sum += nums[j];
+    }
+    //store the sum in sums array
+    sums.push(sum);
+  }
+
+  return sums;
+};
