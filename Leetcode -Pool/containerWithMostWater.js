54c186daedc093fa4adae21d24e01c14eb03966e
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-26 13:16:19 +0500

    Container with most water

---
+++ Leetcode -Pool/containerWithMostWater.js
@@ -0,0 +1,11 @@
+var maxArea = function (height) {
+  let min = 0;
+  let max = 0;
+  let l = 0,
+    r = height.length - 1;
+  while (l < r) {
+    max = Math.max(max, (r - l) * Math.min(height[l], height[r]));
+    height[l] < height[r] ? l++ : r--;
+  }
+  return max;
+};
