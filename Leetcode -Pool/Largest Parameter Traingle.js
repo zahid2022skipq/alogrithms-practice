64e5a1fa1628d59e3323d56c4552b93ebc7d918f
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-29 13:39:25 +0500

    Largest Parameter traingle

---
+++ Leetcode -Pool/Largest Parameter Traingle.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  for (let i = nums.length - 3; i >= 0; --i) {
+    console.log(nums[i] + nums[i + 1] + nums[i + 2]);
+    if (nums[i] + nums[i + 1] > nums[i + 2]) {
+      return nums[i] + nums[i + 1] + nums[i + 2];
+    }
+  }
+  return 0;
+};
