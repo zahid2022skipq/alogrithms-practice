e0811d7796f134ab83a340343f908d474591bf4d
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-29 11:48:07 +0500

    Symmetric tree comments

--- Leetcode -Pool/Symetric Tree.js
+++ Leetcode -Pool/Symetric Tree.js
@@ -11,29 +11,42 @@
  * @return {boolean}
  */
 var isSymmetric = function (root) {
+  // result variable for storing values
   let result = true;
 
+  //check if left and right exists if not return resutl
   if (!root.left && !root.right) return result;
 
-  function traverse(l, r) {
+  // recursive logic for symmetry
+  function trv(l, r) {
+    //check if left and right exist
     if (!l && !r) return;
 
+    //check if left exists and right doesn't or vice-versa
     if ((!l && r) || (!r && l)) {
       result = false;
       return;
     }
 
+    //check if left and right values are same if not make resutl false and return out of function
     if (l.val !== r.val) {
       result = false;
       return;
     }
+
+    //if result is still true
     if (result) {
-      if (l.left || r.right) traverse(l.left, r.right);
-      if (l.right || l.left) traverse(l.right, r.left);
+      //if left and right for l and r exists call the recursive function
+      if (l.left || r.right) trv(l.left, r.right);
+      //check if right and left exist for l and r call the recursive function
+      if (l.right || l.left) trv(l.right, r.left);
     }
+    // after each call return out of the function
     return;
   }
 
-  traverse(root.left, root.right);
+  // call the recursive function
+  trv(root.left, root.right);
+  // return result
   return result;
 };
