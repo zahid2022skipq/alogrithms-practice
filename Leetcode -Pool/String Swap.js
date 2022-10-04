bf72e3fc63b755e28cd75b2096a6a42694705ca1
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-10-04 11:27:56 +0500

    String Swap

--- Leetcode -Pool/String Swap.js
+++ Leetcode -Pool/String Swap.js
@@ -1,52 +1,19 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
 var areAlmostEqual = function (s1, s2) {
-  //check if s1 length and s2 length is not equal return false
+  if (s1 === s2) return true;
   if (s1.length !== s2.length) return false;
-  if (s1 == s2) return true;
-  let str1 = s1.split(""),
-    str2 = s2.split("");
-
-  let i = 0,
-    j = 0;
-  while (i < str1.length) {
-    if (j === str1.length) {
-      j = 0;
-      i++;
+  let ind = [];
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] !== s2[i]) {
+      ind.push(i);
     }
-
-    if (s1[i] === s2[j]) {
-      let temp = str2[i];
-      str2[i] = str2[j];
-      str2[j] = temp;
-
-      if (str2.join("") == str1.join("")) {
-        return true;
-      }
-      i++;
-      j++;
-      (str1 = s1.split("")), (str2 = s2.split(""));
-      continue;
-    }
-
-    j++;
+    if (ind.length > 2) return false;
   }
-  return false;
+  if (ind.length < 2) return false;
+
+  return s1[ind[0]] === s2[ind[1]] && s1[ind[1]] === s2[ind[0]] ? true : false;
 };
-// let s1 = "attack",
-//   s2 = "defend";
-// console.log(areAlmostEqual("bank", "kanb"));
-// console.log(areAlmostEqual(s1, s2));
-// (s1 = "kelb"), (s2 = "kelb");
-// console.log(areAlmostEqual(s1, s2));
-// s1 = "abcd";
-// s2 = "dcba";
-s1 =
-  "ysmpagrkzsmmzmsssutzgpxrmoylkgemgfcperptsxjcsgojwourhxlhqkxumonfgrczmjvbhwvhpnocz";
-s2 =
-  "ysmpagrqzsmmzmsssutzgpxrmoylkgemgfcperptsxjcsgojwourhxlhkkxumonfgrczmjvbhwvhpnocz";
-console.log(areAlmostEqual(s1, s2));
-
-for (let i = 0; i < s1.length; i++) {
-  if (s1[i] !== s2[i]) {
-    console.log(i);
-  }
-}
