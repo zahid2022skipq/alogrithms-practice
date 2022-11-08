896491586b1762870a11ff33f63ce7e6f63274e0
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-08 14:50:16 +0500

    min cost climbing stairs

--- Leetcode -Pool/Min Cost Climbing.js
+++ Leetcode -Pool/Min Cost Climbing.js
@@ -1,46 +1,16 @@
-/**
- * @param {number[]} cost
- * @return {number}
- */
 var minCostClimbingStairs = function (cost) {
-  let currCost = 0;
-  let alter = 0;
+  if (cost.length === 1) return cost[0];
+  if (cost.length === 2) return cost[0] < cost[1] ? cost[0] : cost[1];
 
-  currCost = cost[0];
-  for (let i = 0; i < cost.length; i++) {
-    let flag = cost[i + 1] !== undefined || cost[i + 2] !== undefined;
-    if (cost[i + 1] < cost[i + 2] && flag) {
-      currCost += cost[i + 1];
-      continue;
-    } else if (cost[i + 1] === cost[i + 2] && flag) {
-      currCost += cost[i + 2];
-      i++;
-      continue;
-    } else {
-      if (!flag) break;
-      currCost += cost[i + 2];
-      i++;
-      continue;
-    }
+  let minCost = cost[0];
+  let alter = cost[1];
+
+  for (let i = 2; i < cost.length; i++) {
+    const currMinCost = cost[i] + Math.min(minCost, alter);
+
+    minCost = alter;
+    alter = currMinCost;
   }
-  alter = cost[1];
-  for (let i = 1; i < cost.length; i++) {
-    let flag = cost[i + 1] !== undefined || cost[i + 2] !== undefined;
-    if (cost[i + 1] < cost[i + 2] && flag) {
-      alter += cost[i + 1];
-      continue;
-    } else if (cost[i + 1] === cost[i + 2] && flag) {
-      alter += cost[i + 2];
-      i++;
-      continue;
-    } else {
-      if (!flag) break;
-      alter += cost[i + 2];
-      i++;
-      continue;
-    }
-  }
-  return Math.min(alter, currCost);
+
+  return minCost <= alter ? minCost : alter;
 };
-
-console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]));
